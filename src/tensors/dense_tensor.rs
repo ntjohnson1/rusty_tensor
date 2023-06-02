b3d059cb7a2070e0db5f96c292ef2d0a262d6afa
Nick Johnson <24689722+ntjohnson1@users.noreply.github.com>
2023-06-02 08:22:04 -0400

    Add basic support for norm, ndims, and innerprod

--- src/tensors/dense_tensor.rs
+++ src/tensors/dense_tensor.rs
@@ -1,4 +1,5 @@
-use ndarray::{Array, IxDyn, ShapeBuilder};
+use ndarray::{Array, IxDyn};
+use ndarray_linalg::Norm;
 
 #[derive(Debug)]
 pub struct DenseTensor {
@@ -31,6 +32,22 @@ impl DenseTensor {
             }
         }
     }
+
+    pub fn ndims(&self) -> usize {
+        self.shape.len()
+    }
+
+    pub fn norm(&self) -> f64 {
+        self.data.norm()
+    }
+
+    pub fn innerprod(&self, other: &DenseTensor) -> f64 {
+        // TODO check shape
+        // TODO This makes a flat copy, check if we can do it with view
+        let flat_self = Array::from_iter(self.data.iter().cloned());
+        let flat_other = Array::from_iter(other.data.iter().cloned());
+        flat_self.dot(&flat_other)
+    }
 }
 
 #[cfg(test)]
@@ -56,5 +73,35 @@ mod tests {
         let tensor = DenseTensor::from_data(&array, &Some(shape));
         assert!(!tensor.shape.is_empty());
         assert!(!tensor.data.is_empty());
+    }
+
+    #[test]
+    fn ndims() {
+        let tensor = DenseTensor::new();
+        assert!(tensor.ndims() == 0);
+
+        let array = Array::<f64, IxDyn>::zeros(IxDyn(&[3, 3]));
+        let tensor = DenseTensor::from_data(&array, &None);
+        assert!(tensor.ndims() == 2);
+    }
+
+    #[test]
+    fn norm() {
+        let tensor = DenseTensor::new();
+        assert!(tensor.norm() == 0.0);
+
+        let array = Array::<f64, IxDyn>::ones(IxDyn(&[3, 3]));
+        let tensor = DenseTensor::from_data(&array, &None);
+        assert!(tensor.norm() == 3.0);
+    }
+
+    #[test]
+    fn innerprod() {
+        let tensor = DenseTensor::new();
+        assert!(tensor.innerprod(&tensor) == 0.0);
+
+        let array = Array::<f64, IxDyn>::ones(IxDyn(&[3, 3, 3]));
+        let tensor = DenseTensor::from_data(&array, &None);
+        assert!(tensor.innerprod(&tensor) == 27.0);
     }
 }
