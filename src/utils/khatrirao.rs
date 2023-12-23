eb6a74d19e751cd7317845f86810498b6343fda1
Nick Johnson <24689722+ntjohnson1@users.noreply.github.com>
2023-12-23 11:57:38 -0500

    Fix khatrirao shapping for non-matrix data

--- src/utils/khatrirao.rs
+++ src/utils/khatrirao.rs
@@ -30,17 +30,18 @@ pub fn khatrirao(factors: &[Array<f64, Ix2>], reverse: &Option<bool>) -> Array<f
 
     // Now lets compute something
     let p = factor_it.next().unwrap().clone(); // Grab the first and iterate over the rest
-    let p_rows = p.shape()[0];
+    let mut p_remaining = p.shape().iter().product::<usize>() / ncol_first;
     let mut p = p.insert_axis(ndarray::Axis(0));
     for i in factor_it {
         let i_rows = i.shape()[0];
         let i = i.clone().insert_axis(ndarray::Axis(1));
         let i = i.into_shape((i_rows, 1, ncol_first)).unwrap();
-        p = p.into_shape((1, p_rows, ncol_first)).unwrap();
+        p = p.into_shape((1, p_remaining, ncol_first)).unwrap();
         p = i * p;
+        p_remaining = p.shape().iter().product::<usize>() / ncol_first;
     }
     // TODO explore a helper for creating a Shape based on all but one dim
-    let p_remaining = p.shape().iter().product::<usize>() / ncol_first;
+    p_remaining = p.shape().iter().product::<usize>() / ncol_first;
     // TODO make everything above COW array until here
     p.to_shape((p_remaining, ncol_first)).unwrap().to_owned()
 }
