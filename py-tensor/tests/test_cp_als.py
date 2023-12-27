7109a44cc8fae1aaf62d006ca586fa997e00c0c9
Nick Johnson <24689722+ntjohnson1@users.noreply.github.com>
2023-12-27 15:26:20 -0500

    Add simple regression test against pyttb

--- py-tensor/tests/test_cp_als.py
+++ py-tensor/tests/test_cp_als.py
@@ -1,6 +1,7 @@
 from tensor_ext import Dense, Kruskal, cp_als
 import pytest
 import numpy as np
+import pyttb as ttb
 
 def test_constructor():
     data = np.array([[29.0, 39.0], [63.0, 85.0]])
@@ -14,3 +15,18 @@ def test_constructor():
     k = Kruskal(weights, factors)
     result = cp_als(d, k, rank)
     np.testing.assert_allclose(result.full().data, data)
+
+def test_comparison():
+    rank = 2
+    shape = (4, 4, 4)
+    data = np.random.random(shape)
+    weights = np.ones((rank,))
+    factors = tuple(np.random.random((first, rank)) for first in shape)
+    py_tensor = ttb.tensor(data)
+    py_ktensor = ttb.ktensor(list(factors), weights)
+    rust_dense = Dense(data)
+    rust_kruskal = Kruskal(weights, factors)
+
+    pyttb_result, _, _ = ttb.cp_als(py_tensor, rank, init=py_ktensor, printitn=1)
+    rusty_result = cp_als(rust_dense, rust_kruskal, rank)
+    np.testing.assert_allclose(pyttb_result.full().data, rusty_result.full().data)
